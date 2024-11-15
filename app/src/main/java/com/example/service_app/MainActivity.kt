46f4c777e49555a8aaece301cf771209f5a990c3
rizz175 <rizzikhan175@gmail.com>
2024-11-15 14:45:12 +0500

    created bottom navigation

--- app/src/main/java/com/example/service_app/MainActivity.kt
+++ app/src/main/java/com/example/service_app/MainActivity.kt
@@ -4,24 +4,50 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.service_app.R
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNav)
+        bottomNavigationView.selectedItemId = R.id.nav_home
 
-        // Start the background service
+        // Set the listener for tab selections
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+
+            when (item.itemId) {
+                R.id.nav_home -> fragment = HomeFragment()
+                R.id.nav_search -> fragment = SearchFragment()
+                R.id.nav_notifications -> fragment = NotifiactionsFragment()
+                R.id.nav_profile -> fragment = ProfileFragment()
+            }
+
+            // Replace the fragment when a tab is selected
+            if (fragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit()
+            }
+
+            true
+        }
+
         val serviceIntent = Intent(this, MyService::class.java)
         startService(serviceIntent)
         Log.d("MainActivity", "Service Started from MainActivity")
     }
     override fun onDestroy() {
         super.onDestroy()
-        // Optionally, you can stop the service when the activity is destroyed
         val serviceIntent = Intent(this,  MyService::class.java)
         stopService(serviceIntent)
         Log.d("MainActivity", "Service Stopped from MainActivity")
     }
+
+
 }
