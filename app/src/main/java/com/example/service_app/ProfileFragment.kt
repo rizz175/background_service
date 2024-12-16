a52703aff2fc8c8a6584de676fce1fc8dba888bc
muhammadrizwan <rizzikhan175@gmail.com>
2024-12-16 22:34:28 +0500

    created appcompat spinner with populated data

--- app/src/main/java/com/example/service_app/ProfileFragment.kt
+++ app/src/main/java/com/example/service_app/ProfileFragment.kt
@@ -1,40 +1,42 @@
 package com.example.service_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var spinner: AppCompatSpinner
+    private lateinit var adapter: ArrayAdapter<String>
+    private val items = mutableListOf("Item 1", "Item 2", "Item 3", "Item 4")
+    private lateinit var clearButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        spinner = view.findViewById(R.id.spinner)
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, items).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        spinner.adapter = adapter
+        clearButton = view.findViewById(R.id.clear_button)
+        clearButton.setOnClickListener {
+            clearSpinnerData()
+        }
+        return view
+    }
+
+
+    private fun clearSpinnerData() {
+        items.clear()
+        adapter.notifyDataSetChanged()
     }
 
     companion object {
@@ -46,14 +48,6 @@ class ProfileFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ProfileFragment.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
