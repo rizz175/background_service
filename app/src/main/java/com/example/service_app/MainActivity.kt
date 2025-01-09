69a55b12f7c877dfa9d5c437b9b691fce78e1825
muhammadrizwan <rizzikhan175@gmail.com>
2025-01-10 00:11:38 +0500

    popup created

--- app/src/main/java/com/example/service_app/MainActivity.kt
+++ app/src/main/java/com/example/service_app/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.service_app;
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.PopupWindow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.service_app.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.mainButton.setOnClickListener {
+            showPopup(it)
+        }
 
         bottomNavigationView = findViewById(R.id.bottomNav)
         bottomNavigationView.selectedItemId = R.id.nav_profile
@@ -58,7 +73,44 @@ class MainActivity : AppCompatActivity() {
         stopService(serviceIntent)
         Log.d("MainActivity", "Service Stopped from MainActivity")
     }
+    private fun showPopup(anchorView: View) {
+        val popupView = layoutInflater.inflate(R.layout.popup_layout, null)
 
+        popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val width = popupView.measuredWidth
+        val height = popupView.measuredHeight
+
+        Log.d("MainActivity", "Popup dimensions - Width: $width, Height: $height")
+
+        val popupWindow = PopupWindow(
+            popupView,
+            width,
+            height,
+            true
+        )
+
+        popupView.findViewById<ImageView>(R.id.item1).setOnClickListener {
+            Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show()
+        }
+        popupView.findViewById<ImageView>(R.id.item2).setOnClickListener {
+            Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show()
+        }
+        popupView.findViewById<ImageView>(R.id.item3).setOnClickListener {
+            Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show()
+        }
+        popupView.findViewById<ImageView>(R.id.item4).setOnClickListener {
+            Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show()
+        }
+        popupView.findViewById<ImageView>(R.id.item5).setOnClickListener {
+            Toast.makeText(this, "Item 5 clicked", Toast.LENGTH_SHORT).show()
+        }
+        popupView.findViewById<ImageView>(R.id.item6).setOnClickListener {
+            Toast.makeText(this, "Item 6 clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        popupWindow.showAsDropDown(anchorView)
+
+    }
     fun parseACL() {
         val result = mutableMapOf<String, Map<String, Int>>()
 
